53bf1eea58a86030c995fb50bc58d6a8f261fd3a
songjialin <songjialin@50lion.com>
2020-10-26 17:33:29 +0800

    init

---
+++ src/main/java/com/github/yungyu16/commom/toolkit/base/NamedThreadFactory.java
@@ -0,0 +1,42 @@
+package com.github.yungyu16.commom.toolkit.base;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+
+    private static final AtomicInteger poolNumber = new AtomicInteger(1);
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final ThreadGroup group;
+    private final String namePrefix;
+    private final boolean isDaemon;
+
+    private NamedThreadFactory(String prefix, boolean daemon) {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+        namePrefix = prefix + "-" + poolNumber.getAndIncrement() + "-thread-";
+        isDaemon = daemon;
+    }
+
+    public static ThreadFactory newInstance() {
+        return newInstance("threadPool");
+    }
+
+    public static ThreadFactory newInstance(String prefix) {
+        return newInstance(prefix, false);
+    }
+
+    public static ThreadFactory newInstance(String prefix, boolean daemon) {
+        return new NamedThreadFactory(prefix, daemon);
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+        t.setDaemon(isDaemon);
+        if (t.getPriority() != Thread.NORM_PRIORITY) {
+            t.setPriority(Thread.NORM_PRIORITY);
+        }
+        return t;
+    }
+}
